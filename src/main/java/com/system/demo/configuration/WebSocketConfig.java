49e498850d3ba2a0ee1a1f20e61b48ee08bab569
felix0821 <felixmontalico@gmail.com>
2022-01-18 14:59:09 -0500

    Proyecto Deploy Alertas

--- src/main/java/com/system/demo/configuration/WebSocketConfig.java
+++ src/main/java/com/system/demo/configuration/WebSocketConfig.java
@@ -13,7 +13,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/alert-websocket")
-		.setAllowedOrigins("*")
 		.withSockJS();
 	}
 	
