f9d4f11c64b1a08d90659f52e4a4bd55ff189c0b
felix0821 <felixmontalico@gmail.com>
2022-01-26 15:18:57 -0500

    Deploy Funcionalidad Matricula

--- src/main/java/com/system/demo/service/impl/RequisitionDataDetailServiceImplements.java
+++ src/main/java/com/system/demo/service/impl/RequisitionDataDetailServiceImplements.java
@@ -1,11 +1,16 @@
 package com.system.demo.service.impl;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.system.demo.model.RequisitionDataDetail;
 import com.system.demo.repository.RequisitionDataDetailRepository;
 import com.system.demo.service.RequisitionDataDetailService;
 
+@Service
+@Transactional
 public class RequisitionDataDetailServiceImplements implements RequisitionDataDetailService {
 	
 	@Autowired
