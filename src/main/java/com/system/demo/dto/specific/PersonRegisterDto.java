44a67dce96f201deef8f18dca29012c32bf4cabb
felix0821 <felixmontalico@gmail.com>
2022-03-01 09:07:36 -0500

    Funcionalidad Registro Matricula

--- src/main/java/com/system/demo/dto/specific/PersonRegisterDto.java
+++ src/main/java/com/system/demo/dto/specific/PersonRegisterDto.java
@@ -1,25 +1,23 @@
 package com.system.demo.dto.specific;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class PersonRegisterDto {
 	
 	@NotBlank(message = "Ingrese un nombre de usuario.")
-	@Max(value = 128, message = "Él limite de carácteres para el nombre de usuario no debe sobrepasar los 128.")
-	@Min(value = 5, message = "El nombre de usuario necesita al menos 5 caracteres.")
+	@Size(max = 128, message = "Él limite de carácteres para el nombre de usuario no debe sobrepasar los 128.")
+	@Size(min = 6, message = "El nombre de usuario necesita al menos 5 caracteres.")
 	private String username;
 	
 	@NotBlank(message = "Ingrese una contraseña.")
-	@Max(value = 128, message = "Él limite de carácteres para la contraseña no debe sobrepasar los 128.")
-	@Min(value = 6, message = "La contraseña necesita al menos 6 caracteres.")
+	@Size(max = 128, message = "Él limite de carácteres para la contraseña no debe sobrepasar los 128.")
+	@Size(min = 6, message = "La contraseña necesita al menos 6 caracteres.")
 	private String password;
 	
 	@NotBlank(message = "Ingrese un correo electronico")
-	@Max(value = 128, message = "Él limite de carácteres para el correo electrónico no debe sobrepasar los 128.")
+	@Size(max = 128, message = "Él limite de carácteres para el correo electrónico no debe sobrepasar los 128.")
 	@Email(message = "Correo electrónico invalido")
 	private String email;
 	
