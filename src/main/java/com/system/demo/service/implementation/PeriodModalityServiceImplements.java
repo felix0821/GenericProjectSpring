342ddc76c8492373b2d0a75995e7c3cf6f99689e
felix0821 <felixmontalico@gmail.com>
2022-03-02 05:45:56 -0500

    Funcionalidad Registro Matricula

--- src/main/java/com/system/demo/service/implementation/PeriodModalityServiceImplements.java
+++ src/main/java/com/system/demo/service/implementation/PeriodModalityServiceImplements.java
@@ -1,11 +1,16 @@
 package com.system.demo.service.implementation;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.system.demo.persistence.entity.PeriodModality;
 import com.system.demo.persistence.repository.PeriodModalityRepository;
 import com.system.demo.service.PeriodModalityService;
 
+@Service
+@Transactional
 public class PeriodModalityServiceImplements implements PeriodModalityService {
 	
 	@Autowired
